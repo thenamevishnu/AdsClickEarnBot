249c8c38b18829215b630667b78f55ccd2a3f819
thenamevishnu <mail.vishnumk@gmail.com>
2024-05-12 20:16:56 +0530

    update in polling error

--- Config/Telegram.mjs
+++ Config/Telegram.mjs
@@ -1,5 +1,10 @@
 import Telegram from "node-telegram-bot-api"
 
-const api = new Telegram(process.env.BOT_TOKEN, { polling: true })
+const api = new Telegram(process.env.BOT_TOKEN, { polling: true, request: {
+    agentOptions: {
+        keepAlive: true,
+        family: 4
+    }
+}})
 
 export default api
