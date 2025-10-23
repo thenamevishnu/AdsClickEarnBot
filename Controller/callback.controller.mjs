4b872654cea5c62c1e54f9dae2afdea41e046712
VISHNU M K <mail.vishnumk@gmail.com>
2025-10-23 16:25:34 +0530

    Updated mailing

--- Controller/callback.controller.mjs
+++ Controller/callback.controller.mjs
@@ -922,9 +922,8 @@ api.on("callback_query", async callback => {
             messageStat.sent = 0
             messageStat.failed = 0
             messageStat.success = 0
-            await api.editMessageText(text, {
-                chat_id: from.id,
-                message_id: callback.message.message_id,
+            await api.deleteMessage(from.id, callback.message.message_id)
+            await api.sendMessage(from.id, text, {
                 parse_mode: "HTML",
                 protect_content: settings.PROTECTED_CONTENT,
                 reply_markup: {
